28c336c4ebee35a413980c00b9cd39bdd85f3bca
Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2022-09-07 19:47:20 +0300

    added input functions

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 def get_choices():
-  player_choice = "rock"                                                                                                                                   
-  computer_choice = "paper"
-  return player_choice
-  
+  player_choice = input("Enter a Choice(rock, paper, scissors)")                                                                                                                                 
+  computer_choice = input("Enyer a Computer Choice")
+  choices = { "player" : player_choice, "computer" : computer_choice}
+  return choices
+
 def greeting():
   return "Hi"
+
 choice = get_choices()
 print(choice)
+
 # Gets the greeting then prints it out
 response = greeting()
 print(response)
+
+
+dict = {"name": "Brian", "color": choice}
