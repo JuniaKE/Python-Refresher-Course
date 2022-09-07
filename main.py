8a3f77f458a12be09048dc43a409ac241101eba6
Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2022-09-07 19:39:28 +0300

    getting started with functions and variables

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_choices():
+  player_choice = "rock"                                                                                                                                   
+  computer_choice = "paper"
+  return player_choice
+  
+def greeting():
+  return "Hi"
+choice = get_choices()
+print(choice)
+# Gets the greeting then prints it out
+response = greeting()
+print(response)
